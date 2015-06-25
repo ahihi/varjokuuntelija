b18af914392a0e534c2141619e3cc06f814e7318
Miranda Kastemaa <miranda@foldplop.com>
2015-06-26 00:23:10 +0300

    Implement basic rect rendering with shaders

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,124 @@
 extern crate gl;
+extern crate glmoi;
 extern crate glutin;
 extern crate libc;
 
+use gl::types::*;
+use std::mem;
+use std::ptr;
+use std::ffi::CString;
+use glmoi::shaders::{Program, Shader};
+
+static VS_SRC: &'static str =
+   "#version 150\n\
+    in vec3 position;\n\
+    void main() {\n\
+       gl_Position = vec4(position, 1.0);\n\
+    }";
+
+static FS_SRC: &'static str =
+   "#version 150\n\
+    uniform vec2 u_resolution;\n\
+    out vec4 out_color;\n\
+    void main() {\n\
+       out_color = vec4(gl_FragCoord.x/u_resolution.x, 0.0, gl_FragCoord.y/u_resolution.y, 1.0);\n\
+    }";
+
+fn str_ptr(s: &str) -> *const i8 {
+    CString::new(s).unwrap().as_ptr()
+}
+
 fn main() {
     let window = glutin::Window::new().unwrap();
-
     let _ = unsafe { window.make_current() };
+    gl::load_with(|symbol| window.get_proc_address(symbol));
+    
+    let vs = Shader::new(VS_SRC, gl::VERTEX_SHADER);
+    let fs = Shader::new(FS_SRC, gl::FRAGMENT_SHADER);
+    let program = Program::new(&vs, &fs);
 
+    let fs_resolution_loc = unsafe { gl::GetUniformLocation(program.id, str_ptr("u_resolution")) };
+
+    let mut vao = 0;
+    let mut vbo = 0;
+
+    let vertices: [GLfloat; 12] = [
+        -1.0, -1.0, 0.0,
+         1.0, -1.0, 0.0,
+         1.0,  1.0, 0.0,
+        -1.0,  1.0, 0.0
+    ];
+    
     unsafe {
-        gl::load_with(|symbol| window.get_proc_address(symbol));
+        // Create Vertex Array Object
+        gl::GenVertexArrays(1, &mut vao);
+        gl::BindVertexArray(vao);
 
-        gl::ClearColor(0.0, 1.0, 0.0, 1.0);
+        // Create a Vertex Buffer Object and copy the vertex data to it
+        gl::GenBuffers(1, &mut vbo);
+        gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
+        gl::BufferData(gl::ARRAY_BUFFER,
+                       (vertices.len() * mem::size_of::<GLfloat>()) as GLsizeiptr,
+                       mem::transmute(&vertices[0]),
+                       gl::STATIC_DRAW);
+
+        // Use shader program
+        program.enable();
+        gl::BindFragDataLocation(program.id, 0, str_ptr("out_color"));
+
+        // Specify the layout of the vertex data
+        let pos_attr = gl::GetAttribLocation(program.id, str_ptr("position"));
+        gl::EnableVertexAttribArray(pos_attr as GLuint);
+        gl::VertexAttribPointer(pos_attr as GLuint, 3, gl::FLOAT,
+                                gl::FALSE as GLboolean, 0, ptr::null());
     }
 
     for event in window.wait_events() {
-        unsafe { gl::Clear(gl::COLOR_BUFFER_BIT) };
+        unsafe {
+            let (width, height) = match window.get_inner_size() {
+                Some(sz) => sz,
+                None     => (0, 0)
+            };
+            gl::Uniform2f(fs_resolution_loc, width as GLfloat, height as GLfloat);
+                        
+            // Clear the screen to black
+            gl::ClearColor(0.0, 0.0, 0.0, 1.0);
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+
+            // Draw
+            gl::DrawArrays(gl::TRIANGLE_FAN, 0, (vertices.len()) as i32);
+            
+        };
         let _ = window.swap_buffers();
 
         match event {
             glutin::Event::Closed => break,
-            _ => ()
+            glutin::Event::KeyboardInput(glutin::ElementState::Pressed, _, Some(glutin::VirtualKeyCode::Escape)) => break,
+            _ => println!("{:?}", event)
         }
     }
 }
+
+/*fn main2() {
+    let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
+
+    // Choose a GL profile that is compatible with OS X 10.7+
+    glfw.window_hint(WindowHint::ContextVersion(3, 2));
+    glfw.window_hint(WindowHint::OpenGlForwardCompat(true));
+    glfw.window_hint(WindowHint::OpenGlProfile(OpenGlProfileHint::Core));
+
+    let (mut window, _) = glfw.create_window(800, 600, "OpenGL", WindowMode::Windowed)
+        .expect("Failed to create GLFW window.");
+
+    // It is essential to make the context current before calling `gl::load_with`.
+    window.make_current();
+
+    unsafe {
+    // Cleanup
+        gl::DeleteProgram(program);
+        gl::DeleteShader(fs);
+        gl::DeleteShader(vs);
+        gl::DeleteBuffers(1, &vbo);
+        gl::DeleteVertexArrays(1, &vao);
+    }
+}*/
---
+++ src/shaders.rs
@@ -0,0 +1,97 @@
+extern crate gl;
+//extern crate libc;
+
+use self::gl::types::*;
+use std::ptr;
+use std::str;
+use std::ffi::CString;
+
+pub struct Shader {
+    pub id: GLuint,
+    kind: GLenum
+}
+
+impl Shader {
+    pub fn new(src: &str, kind: GLenum) -> Shader {
+        unsafe {
+            let shader = gl::CreateShader(kind);
+            
+            // Attempt to compile the shader
+            let c_str = CString::new(src.as_bytes()).unwrap();
+            gl::ShaderSource(shader, 1, &c_str.as_ptr(), ptr::null());
+            gl::CompileShader(shader);
+
+            // Get the compile status
+            let mut status = gl::FALSE as GLint;
+            gl::GetShaderiv(shader, gl::COMPILE_STATUS, &mut status);
+
+            // Fail on error
+            if status != (gl::TRUE as GLint) {
+                let mut len = 0;
+                gl::GetShaderiv(shader, gl::INFO_LOG_LENGTH, &mut len);
+                let mut buf = Vec::with_capacity(len as usize);
+                buf.set_len((len as usize) - 1); // subtract 1 to skip the trailing null character
+                gl::GetShaderInfoLog(shader, len, ptr::null_mut(), buf.as_mut_ptr() as *mut GLchar);
+                panic!("{}", str::from_utf8(&buf).ok().expect("ShaderInfoLog not valid utf8"));
+            }
+            
+            Shader {
+                id: shader,
+                kind: kind
+            }
+        }
+    }
+}
+
+impl Drop for Shader {
+    fn drop(&mut self) {
+        unsafe { gl::DeleteShader(self.id); }
+    }
+}
+
+pub struct Program<'a> {
+    pub id: GLuint,
+    vertex_shader: &'a Shader,
+    fragment_shader: &'a Shader
+}
+
+impl<'a> Program<'a> {
+    pub fn new(vertex_shader: &'a Shader, fragment_shader: &'a Shader) -> Program<'a> {
+        unsafe {
+            let program = gl::CreateProgram();
+            gl::AttachShader(program, vertex_shader.id);
+            gl::AttachShader(program, fragment_shader.id);
+            gl::LinkProgram(program);
+            
+            // Get the link status
+            let mut status = gl::FALSE as GLint;
+            gl::GetProgramiv(program, gl::LINK_STATUS, &mut status);
+
+            // Fail on error
+            if status != (gl::TRUE as GLint) {
+                let mut len: GLint = 0;
+                gl::GetProgramiv(program, gl::INFO_LOG_LENGTH, &mut len);
+                let mut buf = Vec::with_capacity(len as usize);
+                buf.set_len((len as usize) - 1); // subtract 1 to skip the trailing null character
+                gl::GetProgramInfoLog(program, len, ptr::null_mut(), buf.as_mut_ptr() as *mut GLchar);
+                panic!("{}", str::from_utf8(&buf).ok().expect("ProgramInfoLog not valid utf8"));
+            }
+            
+            Program {
+                id: program,
+                vertex_shader: vertex_shader,
+                fragment_shader: fragment_shader
+            }
+        }
+    }
+    
+    pub fn enable(&self) {
+        unsafe { gl::UseProgram(self.id); }
+    }
+}
+
+impl<'a> Drop for Program<'a> {
+    fn drop(&mut self) {
+        unsafe { gl::DeleteProgram(self.id); };
+    }
+}
