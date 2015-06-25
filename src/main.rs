126ff456882d2f81fca8fdb702b6f6a257ccffd9
Miranda Kastemaa <miranda@foldplop.com>
2015-06-25 20:55:14 +0300

    Green window :O

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+extern crate gl;
+extern crate glutin;
+extern crate libc;
+
 fn main() {
-  println!("monnimoi");
+    let window = glutin::Window::new().unwrap();
+
+    unsafe { window.make_current() };
+
+    unsafe {
+        gl::load_with(|symbol| window.get_proc_address(symbol));
+
+        gl::ClearColor(0.0, 1.0, 0.0, 1.0);
+    }
+
+    for event in window.wait_events() {
+        unsafe { gl::Clear(gl::COLOR_BUFFER_BIT) };
+        window.swap_buffers();
+
+        match event {
+            glutin::Event::Closed => break,
+            _ => ()
+        }
+    }
 }
